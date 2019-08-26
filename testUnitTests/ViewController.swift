3e6a472bd161b8932c9293b627fd48efa7f2de64
James Tang <james@goodnotesapp.com>
2019-08-26 17:10:06 +0800

    Initial Commit

---
+++ testUnitTests/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testUnitTests
+//
+//  Created by James Tang on 26/8/2019.
+//  Copyright © 2019 James Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
