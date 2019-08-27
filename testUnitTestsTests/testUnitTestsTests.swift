27a2444be27e4697feb57f3736f08c8747943d20
James Tang <james@goodnotesapp.com>
2019-08-27 10:06:14 +0800

    Add Mac target and make sure Unit Test works

--- testUnitTestsTests/testUnitTestsTests.swift
+++ testUnitTestsTests/testUnitTestsTests.swift
@@ -22,6 +22,9 @@ class testUnitTestsTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        #if targetEnvironment(macCatalyst)
+        XCTAssert(false)
+        #endif
     }
 
     func testPerformanceExample() {
